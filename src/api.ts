48e9469c7e1e1424507a2b2adbc5ebf12c58bef2
ShadowGamer1221 <85442315+ShadowGamer1221@users.noreply.github.com>
2023-11-07 18:12:35 +0100

    Testing Cooldown on API for 1 minute

--- src/api.ts
+++ src/api.ts
@@ -40,7 +40,7 @@ app.post('/stock', async (req, res) => {
     if (isCooldownActive) {
         return res.status(429).send({ success: false, msg: 'Request cooldown in effect' });
     }
-    
+
     const { amount } = req.body;
     if(!amount) return res.send({ success: false, msg: 'Missing parameters.' });
 
@@ -48,7 +48,7 @@ app.post('/stock', async (req, res) => {
 
     setTimeout(() => {
         isCooldownActive = false;
-    }, 5 * 60 * 1000); // Set the cooldown to 5 minutes (5 * 60 * 1000 milliseconds)
+    }, 1 * 60 * 1000); // Set the cooldown to 5 minutes (5 * 60 * 1000 milliseconds)
 
     try {
         const receivedMessage = amount;
