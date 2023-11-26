14703ab40b1d4d48cdb03d153c8042dd56190061
ShadowGamer1221 <85442315+ShadowGamer1221@users.noreply.github.com>
2023-11-26 14:24:15 +0100

    Added /demote /currentstock to API for the website

--- src/api.ts
+++ src/api.ts
@@ -198,7 +198,7 @@ app.post('/promote', async (req, res) => {
                 const reasonEmbed = new EmbedBuilder()
                     .setAuthor({ name: 'Karai Logs', iconURL: infoIconUrl })
                     .setColor(mainColor)
-                    .setDescription(`**Staff Member:** API Action\n**Action:** Promote\n**Target:** ${member}\n**Reason:** Promotion to ${nextRole.name}`)
+                    .setDescription(`**Staff Member:** API Action\n**Action:** Promote\n**Target:** ${member}\n**Reason:** Promotion to \`${nextRole.name}\``)
                     .setTimestamp();
                 channelSend.send({ embeds: [reasonEmbed] });
             } catch (error) {
@@ -211,6 +211,95 @@ app.post('/promote', async (req, res) => {
     } catch (error) {
         console.error(error);
         res.send({ success: false, msg: 'Failed to promote user.' });
+    }
+});
+
+app.post('/botstatus', async (req, res) => {
+    try {
+        const { status } = req.body;
+
+        if (status === 'online' || status === 'idle' || status === 'dnd' || status === 'invisible') {
+            await discordClient.user.setStatus(status);
+            res.send({ success: true });
+        } else {
+            res.send({ success: false, msg: 'Invalid status.' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.send({ success: false, msg: 'Failed to set status.' });
+    }
+});
+
+app.post('/demote', async (req, res) => {
+    try {
+        const { userId } = req.body;
+
+        const member = await discordClient.guilds.cache.get('872395463368769576').members.fetch(userId);
+
+        if (!member) {
+            return res.send({ success: false, msg: 'The mentioned user is not in this server.' });
+        }
+
+        const roles = member.roles.cache;
+
+        // Find the current role position
+        const currentRolePosition = member.roles.highest.position;
+
+        // Find the next role above the current one
+        const nextRole = member.guild.roles.cache.find((r) => r.position === currentRolePosition - 1);
+
+        if (!nextRole) {
+            return res.send({ success: false, msg: 'No role found above the current one.' });
+        }
+
+        if (!roles.has(nextRole.id)) {
+            if (nextRole.position <= currentRolePosition) {
+                return res.send({ success: false, msg: 'The specified role must be higher in position than the user\'s current role.' });
+            }
+
+            try {
+                await member.roles.add(nextRole);
+
+                const dmEmbed = new EmbedBuilder()
+                    .setAuthor({ name: 'Karai Crew', iconURL: infoIconUrl })
+                    .setDescription(`You have been demoted to the **${nextRole.name}** role.`)
+                    .setColor(greenColor);
+                member.send({ embeds: [dmEmbed] });
+
+                const successEmbed = new EmbedBuilder()
+                    .setAuthor({ name: 'Success!', iconURL: checkIconUrl })
+                    .setColor(greenColor)
+                    .setDescription(`Successfully demoteted <@${userId}> to the <@&${nextRole.id}> role`);
+                res.send({ success: true, msg: successEmbed.toJSON() });
+
+                const channelSend: TextChannel = await discordClient.channels.fetch('1168628274759471155') as TextChannel;
+                const reasonEmbed = new EmbedBuilder()
+                    .setAuthor({ name: 'Karai Logs', iconURL: infoIconUrl })
+                    .setColor(mainColor)
+                    .setDescription(`**Staff Member:** API Action\n**Action:** Demote\n**Target:** ${member}\n**Reason:** Promotion to \`${nextRole.name}\``)
+                    .setTimestamp();
+                channelSend.send({ embeds: [reasonEmbed] });
+            } catch (error) {
+                console.error(error);
+                res.send({ success: false, msg: `Failed to demote ${member} to the <@&${nextRole.id}> role.` });
+            }
+        } else {
+            res.send({ success: false, msg: `${member} already has the selected role.` });
+        }
+    } catch (error) {
+        console.error(error);
+        res.send({ success: false, msg: 'Failed to demote user.' });
+    }
+});
+
+app.get('/currentstock', async (req, res) => {
+    try {
+        const channelSend: TextChannel = await discordClient.channels.fetch('1171130227213222041') as TextChannel;
+        const message = channelSend.lastMessage;
+        res.send({ success: true, msg: message.content });
+    } catch (error) {
+        console.error(error);
+        res.send({ success: false, msg: 'Failed to get current stock.' });
     }
 });
 
