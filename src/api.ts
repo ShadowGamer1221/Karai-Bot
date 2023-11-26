a41ae16363fce6cf8c0b858be16b09e3e6d2dd44
ShadowGamer1221 <85442315+ShadowGamer1221@users.noreply.github.com>
2023-11-26 17:15:56 +0100

    Added /apply to API

--- src/api.ts
+++ src/api.ts
@@ -3,7 +3,7 @@ import { config } from './config';
 import { logAction } from './handlers/handleLogging';
 import ms from 'ms';
 import { findEligibleRole } from './handlers/handleXpRankup';
-import { EmbedBuilder, TextChannel, time } from 'discord.js';
+import { ButtonBuilder, EmbedBuilder, TextChannel, time, ButtonStyle, ChannelType, PermissionsBitField } from 'discord.js';
 import { discordClient } from './main';
 import { checkIconUrl, infoIconUrl, mainColor, greenColor, redColor } from './handlers/locale';
 import promote from './commands/admin/promote';
@@ -303,6 +303,115 @@ app.post('/demote', async (req, res) => {
     } catch (error) {
         console.error(error);
         res.send({ success: false, msg: 'Failed to demote user.' });
+    }
+});
+
+app.post('/apply', async (req, res) => {
+    try {
+        const { userId } = req.body;
+
+        const memberfetch = await discordClient.guilds.cache.get('872395463368769576').members.fetch(userId);
+        const guild = discordClient.guilds.cache.get('872395463368769576');
+
+        // Define a category for the clan application channels (you may need to create the category first).
+        const categoryID = '1168840654672109629'; // Replace with the actual category ID.
+
+        // Rest of your code goes here...
+        const channelName = `application-${memberfetch.user.tag}`;
+
+        // Get the staff role.
+        const staffRole = guild.roles.cache.get(config.permissions.recruiter[0]);
+
+        if (!staffRole) {
+            // Handle the case where the staff role is not found.
+            console.log('Staff role not found.');
+            return res.send({ success: false, msg: 'Staff role not found.' });
+        }
+
+        // Create the application channel.
+        const applicationChannel = await guild.channels.create({
+            name: channelName,
+            type: ChannelType.GuildText, // Specify the channel type as text.
+            parent: categoryID, // Set the category for the channel.
+            permissionOverwrites: [
+                {
+                    id: memberfetch.id,
+                    allow: [PermissionsBitField.Flags.ViewChannel], // Allow the user to view the channel.
+                },
+                {
+                    id: staffRole.id,
+                    allow: [PermissionsBitField.Flags.ViewChannel], // Allow staff to view the channel.
+                },
+                {
+                    id: guild.roles.everyone,
+                    deny: [PermissionsBitField.Flags.ViewChannel], // Deny everyone else from viewing the channel.
+                },
+            ],
+        });
+
+        // Store the application channel ID for later use.
+        const applicationChannelID = applicationChannel.id;
+
+        // Send a welcome message in the channel.
+        const welcomeEmbed = new EmbedBuilder()
+            .setTitle('Crew Application')
+            .setDescription('Welcome to the crew application process. A recruiter will be with you shortly. Please answer the following questions:\n\n**1.** What is your total bounty right now?\n\n**2.** What is your current level?\n\n**3.** How frequently do you plan to be active each day?\n\n**4.** Why are you interested in becoming a part of our crew?')
+            .setColor(greenColor) // Green color
+            .setAuthor({ name: memberfetch.user.tag, iconURL: memberfetch.displayAvatarURL() });
+
+        await applicationChannel.send({ content: `<@&1109445519052390400>`, embeds: [welcomeEmbed], allowedMentions: { roles: ['1109445519052390400'] }});
+
+        // Create a "Close" button.
+        const closeButton = new ButtonBuilder()
+            .setCustomId('close')
+            .setLabel('Close')
+            .setEmoji('🔒')
+            .setStyle(ButtonStyle.Danger);
+
+        // Send the action row with the "Close" button.
+        await applicationChannel.send({
+            components: [
+                {
+                    type: 1,
+                    components: [
+                        closeButton,
+                    ],
+                },
+            ],
+        });
+
+        // Send a success message mentioning the created ticket channel.
+        const successEmbed = new EmbedBuilder()
+            .setAuthor({ name: 'Success!', iconURL: checkIconUrl }) // Replace with the icon URL.
+            .setColor(greenColor) // Green color
+            .setDescription(`Successfully created your application ticket in ${applicationChannel}!`)
+            .setTimestamp();
+
+        res.send({ success: true, msg: successEmbed.toJSON() });
+
+        // Handle button interactions using the "interactionCreate" event.
+        discordClient.on('interactionCreate', async (interaction) => {
+            if (!interaction.isButton()) return;
+
+            if (interaction.customId === 'close' && interaction.channel?.id === applicationChannelID) {
+                // Handle "Close" button press by deleting the specific application channel.
+                applicationChannel.delete()
+                    .then(() => {
+                        // Send a DM to the user that their application has been closed.
+                        const closeEmbed = new EmbedBuilder()
+                            .setAuthor({ name: 'Closed', iconURL: infoIconUrl }) // Replace with the icon URL.
+                            .setColor(mainColor) // Blue color
+                            .setDescription('Your crew application has been closed. If you have been accepted, you should have received a DM from me about your status. If not, please open a new application to inquire about your application status.')
+                            .setTimestamp();
+
+                        interaction.user.send({ embeds: [closeEmbed] }).catch(console.error);
+                    })
+                    .catch(console.error);
+            }
+        });
+    } catch (error) {
+        console.error(error);
+        res.send({ success: false, msg: 'Internal server error.' });
     }
 });
 
